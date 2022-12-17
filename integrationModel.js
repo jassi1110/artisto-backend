e76e400c26032f0ed29a8ea6f468f3d489c09a68
jassi1110 <jasnoorsingh11@gmail.com>
2022-12-18 04:10:07 +0530

    updated

--- integrationModel.js
+++ integrationModel.js
@@ -5,12 +5,46 @@ const { PythonShell } = require('python-shell');
 exports.recoML = async (name) => {
     return new Promise((res, rej) => {
     obj = []
-    var spawn = require('child_process').spawn;
-    var process = spawn('python', [__dirname + '/model.py',name]);
-    process.stdout.on('data', function (data) {
-        // console.log(data.toString())
-        result = JSON.parse(data)
-        i = 0;
+    // var spawn = require('child_process').spawn;
+    // var process = spawn('python', [__dirname + '/model.py',name]);
+    // process.stdout.on('data', function (data) {
+    //     // console.log(data.toString())
+    //     result = JSON.parse(data)
+    //     i = 0;
+    //         for (var key in result.Correlation) {
+    //             if (i > 3) {
+    //                 break;
+    //             }
+    //             else if (result.Correlation[key] == 1) {
+    //                 continue;
+    //             }
+    //             else {
+    //                 myJSON = {
+    //                     name: key,
+    //                     Correlation: result.Correlation[key],
+    //                     Rating: result.rating[key],
+    //                     "Number of Ratings": result["num of ratings"][key]
+    //                 }
+    //                 obj.push(myJSON);
+    //             }
+    //             i++;
+    //         }
+    //         i = 0;
+    //         res(obj)
+    //     obj = []
+    //     // res();
+
+        let options = {
+            mode: 'text',
+            pythonPath:'python', 
+            pythonOptions: ['-u'],
+            scriptPath: __dirname,
+            args: [name]
+        };
+        PythonShell.run('model.py', options, function (err, result) {
+            if (err) rej(err);
+            result = JSON.parse(result)
+            i = 0;
             for (var key in result.Correlation) {
                 if (i > 3) {
                     break;
@@ -32,48 +66,14 @@ exports.recoML = async (name) => {
             i = 0;
             res(obj)
         obj = []
-    //     // res();
-
-        // let options = {
-        //     mode: 'text',
-        //     pythonPath:'python', 
-        //     pythonOptions: ['-u'],
-        //     scriptPath: './V1.0.0/ML',
-        //     args: [name]
-        // };
-        // PythonShell.run('model.py', options, function (err, result) {
-        //     if (err) rej(err);
-        //     result = JSON.parse(result)
-        //     i = 0;
-        //     for (var key in result.Correlation) {
-        //         if (i > 3) {
-        //             break;
-        //         }
-        //         else if (result.Correlation[key] == 1) {
-        //             continue;
-        //         }
-        //         else {
-        //             myJSON = {
-        //                 name: key,
-        //                 Correlation: result.Correlation[key],
-        //                 Rating: result.rating[key],
-        //                 "Number of Ratings": result["num of ratings"][key]
-        //             }
-        //             obj.push(myJSON);
-        //         }
-        //         i++;
-        //     }
-        //     i = 0;
-        //     res(obj)
-        // obj = []
         });
     })
 
 }
 
-// const helper = async () => {
-//     const d = await this.recoML('Aarya');
-//     console.log(d);
-// }
+const helper = async () => {
+    const d = await this.recoML('Aarya');
+    console.log(d);
+}
 
-// helper();
+helper();

5d02df4829edd6468bf8ab1d675246d24de88d12
jassi1110 <jasnoorsingh11@gmail.com>
2022-12-18 00:06:08 +0530

    model update

--- V1.0.0/ML/model.py
+++ V1.0.0/ML/model.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 import os
 
+name = sys.argv[1]
 def recommendationEngine():
     column_names = ['user_id', 'item_id', 'rating', 'Artist']
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), f".\\jasnoor.csv"), sep=',', names=column_names)
@@ -21,31 +22,37 @@ def recommendationEngine():
 
     ratings.head()
 
-    # Aadish_user_ratings = moviemat['Anuj']
-    Aakash_user_ratings = moviemat['Aarushi']
-    # Aadish_user_ratings.head()
+    Aadish_user_ratings = moviemat[name]
+    #Aakash_user_ratings = moviemat['Divyesh']
+    Aadish_user_ratings.head()
 
-    # similar_to_Aadish = moviemat.corrwith(Aadish_user_ratings)
-    similar_to_Aakash = moviemat.corrwith(Aakash_user_ratings)
+    similar_to_Aadish = moviemat.corrwith(Aadish_user_ratings)
+    #similar_to_Aakash = moviemat.corrwith(Aakash_user_ratings)
 
-    # corr_Aadish = pd.DataFrame(similar_to_Aadish,columns=['Correlation'])
-    # corr_Aadish.dropna(inplace=True)
-    # corr_Aadish.head()
+    corr_Aadish = pd.DataFrame(similar_to_Aadish,columns=['Correlation'])
+    corr_Aadish.dropna(inplace=True)
+    corr_Aadish.head()
+    corr_Aadish.sort_values('Correlation',ascending=False).head(10)
+    corr_Aadish = corr_Aadish.join(ratings['num of ratings'])
+    corr_Aadish = corr_Aadish.join(ratings['rating'])
 
-    # corr_Aadish.sort_values('Correlation',ascending=False).head(10)
+    corr_Aadish.head()
+    aa=corr_Aadish[corr_Aadish['num of ratings']>100].sort_values('Correlation',ascending=False).sample(n=5)
+    # bb=corr_Aadish.loc['Aarya']
 
-    # corr_Aadish = corr_Aadish.join(ratings['num of ratings'])
-    # corr_Aadish.head()
+    # aa.append(bb)
 
-    # corr_Aadish[corr_Aadish['num of ratings']>100].sort_values('Correlation',ascending=False).head()
 
-    corr_Aakash = pd.DataFrame(similar_to_Aakash,columns=['Correlation'])
-    corr_Aakash.dropna(inplace=True)
-    corr_Aakash = corr_Aakash.join(ratings['num of ratings'])
-    corr_Aakash = corr_Aakash.join(ratings['rating'])
-    corr_Aakash[corr_Aakash['num of ratings']>100].sort_values('Correlation',ascending=False)
-    output = corr_Aakash.head(50)
-    output = output.to_json()
+
+
+
+    # corr_Aakash = pd.DataFrame(similar_to_Aakash,columns=['Correlation'])
+    # corr_Aakash.dropna(inplace=True)
+    # corr_Aakash = corr_Aakash.join(ratings['num of ratings'])
+    # corr_Aakash = corr_Aakash.join(ratings['rating'])
+    # corr_Aakash[corr_Aakash['num of ratings']>100].sort_values('Correlation',ascending=False)
+    output = aa.head().to_json()
+#    output = output.to_json()
     print(output)
     # print("Hello jasnoor")
     sys.stdout.flush()
--- V1.0.0/utils/integrationModel.js
+++ V1.0.0/utils/integrationModel.js
@@ -1,32 +1,20 @@
 const { PythonShell } = require('python-shell');
-exports.recoML = async () => {
+exports.recoML = async (name) => {
     return new Promise((res, rej) => {
         obj = []
         let options = {
             mode: 'text',
             pythonOptions: ['-u'],
-            scriptPath: './V1.0.0/ML'
+            scriptPath: './V1.0.0/ML',
+            args: [name]
         };
 
         PythonShell.run('model.py', options, function (err, result) {
             if (err) rej(err);
-            result = JSON.parse(result[0])
-            for (var key in result.Correlation) {
-                // console.log(key)
-                if (result.Correlation[key] == 1) {
-                    myJSON = {
-                        name: key,
-                        Correlation: result.Correlation[key],
-                        Rating: result.rating[key],
-                        "Number of Ratings": result["num of ratings"][key]
-                    }
-                    obj.push(myJSON);
-                    break;
-                }
-            }
+            result = JSON.parse(result)
             i = 0;
             for (var key in result.Correlation) {
-                if (i > 2) {
+                if (i > 3) {
                     break;
                 }
                 else if (result.Correlation[key] == 1) {
@@ -43,15 +31,18 @@ exports.recoML = async () => {
                 }
                 i++;
             }
+            i = 0;
             res(obj)
+        // res(result)
         });
     })
 
 }
 
 const helper = async () => {
-    const d = await this.recoML();
+    const d = await this.recoML('Aarya');
     console.log(d);
+
 }
 
 helper();

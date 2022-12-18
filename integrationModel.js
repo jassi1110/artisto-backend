8e852af6cd7c3d5a5dc35a16793aa6f2a4d0bc7f
jassi1110 <jasnoorsingh11@gmail.com>
2022-12-18 14:20:58 +0530

    udpated

--- integrationModel.js
+++ integrationModel.js
@@ -101,9 +101,9 @@ exports.recoML = async (name) => {
 
 }
 
-const helper = async () => {
-    const d = await this.recoML('Aarya');
-    console.log(d);
-}
+// const helper = async () => {
+//     const d = await this.recoML('Aarya');
+//     console.log(d);
+// }
 
-helper();
+// helper();
